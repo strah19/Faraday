6319032ff749b15d138da9a66ddf6dfbd3a7b218
strah19 <m.strahinja@yahoo.com>
2023-04-11 17:22:27 -0500

    symbol work

--- include/symbol_table.h
+++ include/symbol_table.h
@@ -1,8 +1,25 @@
 #ifndef symbol_table_h
 #define symbol_table_h
 
-typedef struct {
+typedef union {
+    int integer;
+    float decimal;
+    char* string;
+} SymbolValue;
 
+typedef struct {
+    union {
+        struct Constant {
+            SymbolValue val;
+        };
+
+        
+    } info; 
+} SymbolDefinition;
+
+typedef struct {
+    struct Symbol* left;
+    struct Symbol* right;
 } Symbol;
 
 #endif
